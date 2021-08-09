dcdf5fe1bf5cabf223eeeb7280968a1216d9967d
rodriggo_justino <rodriggo_justino@hotmail.com>
2021-08-09 15:34:16 -0400

    Atualização nos arquivos index.html e teste.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Projeto Devops essentials na 4linux.")
+        self.assertRegex(result.data.decode(), "Projeto Devopsessentials na 4linux")
 
 
 if __name__ == "__main__":
